42dc59dafa4f01358ece368f35ae2459ad514c08
nikhilreddysf1 <nikhilreddy.sf1@gmail.com>
2024-10-06 23:57:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,18 +42,18 @@ if ingredients_list:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
-    #st.write (ingredients_string)
+        #st.write (ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
-    time_to_insert= st.button ('Submit Order')
+        #st.write(my_insert_stmt)
+        #st.stop()
+        time_to_insert= st.button ('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
 
